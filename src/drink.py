ed0439d5d844597e05ea8881eca6874117e0d539
Piotr <pigryko@yahoo.com>
2021-03-03 10:42:14 +0000

    pub class created

---
+++ src/drink.py
@@ -0,0 +1,14 @@
+class Drink:
+
+    # declare data-types
+    name = str
+    price = float
+
+    # initialise Person Class
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    # function example
+    # def name(self):
+    #     pass
